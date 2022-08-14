e9f39780600ecefd0adf9a679af0b70726735b64
asfaquralam1 <ahsansagor449@gmail.com>
2022-08-14 09:17:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -45,9 +45,6 @@ function App() {
 
     const data = await res.json()
     setTasks([...tasks, data])
-    // const id = Math.floor(Math.random() * 1000) + 1
-    // const newTask = { id, ...task }
-    // setTasks([...tasks, newTask])
   }
   //Delete Task
   const deleteTask = async(id) => {
